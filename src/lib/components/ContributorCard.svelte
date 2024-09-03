c09dad7aaae0c22df5adcf355c238d2e72d366f6
conradolandia <andresconrado@gmail.com>
2024-09-03 18:10:03 -0500

    Add ContributorCard component and disable a line in `.gitignore` causing issues with the `$lib` directory

---
+++ src/lib/components/ContributorCard.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let contributor;
+</script>
+
+<div class="flex items-center justify-center">
+  <a href={contributor.html_url} target="_blank" rel="noopener noreferrer">
+    <img
+      src={contributor.avatar_url}
+      alt={contributor.login}
+      class="w-16 h-16 rounded-full mr-4"
+    />
+  </a>
+  <div>
+    <a href={contributor.html_url} target="_blank" rel="noopener noreferrer"
+      >{contributor.login}</a
+    >
+    <p class="text-gray-400 text-sm">
+      {contributor.contributions} contributions
+    </p>
+  </div>
+</div>
