ab58d8954f2bc46705e76bd13fc5aa12a11a18af
conradolandia <andresconrado@gmail.com>
2024-09-03 17:04:01 -0500

    Create ContributorCard and implement data fetching for 48 top contributors (divisible by 2 and 3)

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,4 +1,4 @@
-const dataSrc = `https://api.github.com/repos/spyder-ide/spyder/contributors`;
+const dataSrc = `https://api.github.com/repos/spyder-ide/spyder/contributors?per_page=48`;
 
 export async function load({ fetch }) {
     const response = await fetch(dataSrc);
