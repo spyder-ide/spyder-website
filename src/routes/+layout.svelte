54d7f8b90bbd060acc02c0098d59263e8aea9f34
conradolandia <andresconrado@gmail.com>
2024-09-03 18:36:14 -0500

    Start fixing the metadata preview for social media

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount, onDestroy } from "svelte";
     import { metadata } from "$lib/store";
+
     import {
         title as siteTitle,
         author as siteAuthor,
@@ -27,6 +28,16 @@
     <meta name="description" content={$metadata.description} />
     <meta name="keywords" content={$metadata.keywords} />
     <meta name="author" content={$metadata.author} />
+    <meta name="og:locale" property="og:locale" content="en_US">
+    <meta name="og:site_name" property="og:site_name" content={siteTitle}>
+    <meta name="og:title" property="og:title" content={$metadata.title}>
+    <meta name="og:updated_time" property="og:updated_time" content="1714338554">
+    <meta name="og:type" property="og:type" content="website">
+    <meta name="og:image" property="og:image" content="https://www.spyder-ide.org/assets/media/website_screenshot.webp">
+    <meta name="og:image:width" property="og:image:width" content="1920">
+    <meta name="og:image:height" property="og:image:height" content="995">
+    <meta name="og:url" property="og:url" content="https://www.spyder-ide.org/">
+    <meta name="og:description" property="og:description" content={$metadata.description}>
 </svelte:head>
 
 <div class="layout grid h-full">
