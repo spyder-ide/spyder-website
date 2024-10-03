084dd84808a759d5a958a6f428e5db6339ff4e71
conradolandia <andresconrado@gmail.com>
2024-10-02 21:37:52 -0500

    Try to get back to the old version of hooks.server.js to check if this solves the path issue

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,66 +1,29 @@
-import { join, dirname } from "path";
+import { join } from "path";
 import { existsSync, createReadStream } from "fs";
-import { blogSlug } from "$lib/config";
+import { blogSlug } from "$lib/config"
 
-/**
- * Returns the MIME type based on the file extension.
- * @param {string} filePath - Path to the file.
- * @returns {string} - MIME type.
- */
-function getMimeType(filePath) {
-  const ext = dirname(filePath).split('.').pop().toLowerCase();
-  switch (ext) {
-    case 'png':
-      return 'image/png';
-    case 'jpg':
-    case 'jpeg':
-      return 'image/jpeg';
-    case 'gif':
-      return 'image/gif';
-    case 'svg':
-      return 'image/svg+xml';
-    case 'webp':
-      return 'image/webp';
-    case 'webm':
-      return 'video/webm';
-    case 'mp4':
-      return 'video/mp4';
-    case 'mpogv':
-      return 'video/ogg';
-    case 'mp3':
-      return 'audio/mpeg';
-    case 'ogg':
-      return 'audio/ogg';
-    default:
-      return 'application/octet-stream';
-  }
-}
+export default join;
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-  const imageExtensions = /\.(png|jpe?g|gif|svg|webp|webm|mpogv|mp3|mp4|ogg)$/i;
-
-  // Check if the request is for a media element in a blog post (excluding the OG image)
+  // Check if the request is for a blog media element
   if (
     event.url.pathname.startsWith(`/${blogSlug}/`) &&
-    imageExtensions.test(event.url.pathname)
+    event.url.pathname.match(
+      /\.(png|jpe?g|gif|svg|webp|webm|mpogv|mp3|mp4|ogg)$/i
+    )
   ) {
-    // Construct the full path to the image file within src/routes
+    // Construct the full path to the image file
     const imagePath = join(process.cwd(), "src", "routes", event.url.pathname);
-
     // Check if the image file exists
     if (existsSync(imagePath)) {
+      // Create a read stream for the image file
       const stream = createReadStream(imagePath);
-      const mimeType = getMimeType(imagePath);
-
-      return new Response(stream, {
-        headers: {
-          "Content-Type": mimeType || "application/octet-stream",
-        },
-      });
+      // Return a new Response object with the image stream
+      return new Response(stream);
     }
   }
 
-  // For all other requests, proceed as usual
+  // If the request is not for a blog media element, resolve the event as usual
   return resolve(event);
 }
