b569f35696d920362232a0ad864027ebd9844841
conradolandia <andresconrado@gmail.com>
2024-10-04 13:08:39 -0500

    Try to use a different font copying scheme

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,7 @@
 
 npm install
 mkdir -p ~/.fonts/silka
-cp -r static/assets/fonts/silka/silka-black* ~/.fonts/silka/
-cp -r static/assets/fonts/silka/silka-regular* ~/.fonts/silka/
+cp -r static/assets/fonts/silka/silka-black-* ~/.fonts/silka/
+cp -r static/assets/fonts/silka/silka-regular-* ~/.fonts/silka/
 fc-cache -fv
 fc-list Silka
