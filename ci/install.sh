42047b52892d97a870007c22bfe70f44e0852d8e
conradolandia <andresconrado@gmail.com>
2024-10-04 13:04:19 -0500

    Try again with different settings

--- ci/install.sh
+++ ci/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -ex
 
-sudo apt update && sudo apt install -y fontconfig
 npm install
 mkdir -p ~/.fonts/silka
 cp -r static/assets/fonts/silka/silka-black* ~/.fonts/silka/
 cp -r static/assets/fonts/silka/silka-regular* ~/.fonts/silka/
 fc-cache -fv
+fc-list Silka:Regular
+fc-list Silka:Black
