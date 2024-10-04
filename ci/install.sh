4845399811f160b1c17decd4e955c781fb0a8c9f
conradolandia <andresconrado@gmail.com>
2024-10-04 13:16:52 -0500

    Try embeeding the font in the SVG

--- ci/install.sh
+++ ci/install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -ex
 
 npm install
-mkdir -p ~/.fonts/silka
-cp -r static/assets/fonts/silka/silka-black-* ~/.fonts/silka/
-cp -r static/assets/fonts/silka/silka-regular-* ~/.fonts/silka/
-fc-cache -fv
-fc-list Silka
