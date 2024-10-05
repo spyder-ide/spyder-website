f00f7b07272f17a2cccad4fb04a619406b41187f
conradolandia <andresconrado@gmail.com>
2024-10-04 19:33:15 -0500

    Fix a rookie mistake

--- ci/install.sh
+++ ci/install.sh
@@ -9,7 +9,7 @@ fc-cache -fv
 # Determine if REVIEW_ID is set  to select the correct URL
 if [ -n "$REVIEW_ID" ]; then
   # Staging URL (Deploy Preview)
-  SITE_URL="https://deploy-preview-${REVIEW_ID}--myproject-website-preview.netlify.app/"
+  SITE_URL="https://deploy-preview-${REVIEW_ID}--spyder-svelte-website-preview.netlify.app/"
 elif [ "$NODE_ENV" = "production" ]; then
   # Production URL
   SITE_URL="https://www.spyder-ide.org/"
@@ -23,5 +23,3 @@ cat <<EOF > .env
 PUBLIC_SITE_URL=${SITE_URL}
 EOF
 
-echo "Environment variables have been set in .env"
-cat .env
