7add5ae831d54456af657f3234c8bd35cfd11a18
conradolandia <andresconrado@gmail.com>
2024-10-04 19:42:24 -0500

    Clean up a little bit (we dont need the variable in the shell)

--- ci/install.sh
+++ ci/install.sh
@@ -6,7 +6,7 @@ npm install
 mkdir -p ~/.fonts/silka
 cp -r static/assets/fonts/silka/desktop/* ~/.fonts/silka/
 fc-cache -fv
-# Determine if REVIEW_ID is set  to select the correct URL
+# Determine if REVIEW_ID is set to select the URL to export
 if [ -n "$REVIEW_ID" ]; then
   # Staging URL (Deploy Preview)
   SITE_URL="https://deploy-preview-${REVIEW_ID}--spyder-svelte-website-preview.netlify.app/"
@@ -14,10 +14,6 @@ elif [ "$NODE_ENV" = "production" ]; then
   # Production URL
   SITE_URL="https://www.spyder-ide.org/"
 fi
-
-# Export variables for the current shell
-export PUBLIC_SITE_URL="$SITE_URL"
-
 # Create a .env file for Vite
 cat <<EOF > .env
 PUBLIC_SITE_URL=${SITE_URL}
