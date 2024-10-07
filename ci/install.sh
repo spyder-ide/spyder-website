9560fdf0a1284d9b896dc0124c521beae546aaf0
conradolandia <andresconrado@gmail.com>
2024-10-07 14:11:59 -0500

    Try a new setting in the install script to check an environment variable

--- ci/install.sh
+++ ci/install.sh
@@ -2,10 +2,12 @@
 
 # Install project dependencies
 npm install
+
 # Install fonts for OpenGraph image creation
 mkdir -p ~/.fonts/silka
 cp -r static/assets/fonts/silka/desktop/* ~/.fonts/silka/
 fc-cache -fv
+
 # Determine if REVIEW_ID is set to select the URL to export
 if [ -n "$REVIEW_ID" ]; then
   # Staging URL (Deploy Preview)
@@ -14,6 +16,9 @@ elif [ "$NODE_ENV" = "production" ]; then
   # Production URL
   SITE_URL="https://www.spyder-ide.org/"
 fi
+
+export PUBLIC_SITE_URL=$SITE_URL
+
 # Create a .env file for Vite
 cat <<EOF > .env
 PUBLIC_SITE_URL=${SITE_URL}
