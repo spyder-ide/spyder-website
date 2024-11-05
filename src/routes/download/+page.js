f9f19232874b5e38a4de8cffd1e6090e6b0dac0c
conradolandia <andresconrado@gmail.com>
2024-11-05 14:22:22 -0500

    Correct text following suggestions in issue 31

--- src/routes/download/+page.js
+++ src/routes/download/+page.js
@@ -1,9 +1,9 @@
-const visit = `visit our <a href="https://github.com/spyder-ide/spyder/releases" target="_blank">releases page</a> on Github.`
+const visit = `visit our <a href="https://github.com/spyder-ide/spyder/releases" target="_blank">releases page</a> on GitHub.`
 
 const content = {
   props: {
     title: "Download Spyder",
-    intro: `Please select the package you want from the links below, or ${visit}`,
+    intro: `Select the package you want to download from the links below. Or ${visit}`,
     download: {
       start: "Download started&hellip;",
       click: `If the download does not start automatically, please click the button below`,
