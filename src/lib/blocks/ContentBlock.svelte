689dea8bcd21ea390145c0313659b745e99653ed
Andrés Conrado Montoya Acosta <andresconrado@gmail.com>
2025-01-04 17:20:40 -0500

    Fix buttons size and implement optional textSize variable for Button props (#40)

--- src/lib/blocks/ContentBlock.svelte
+++ src/lib/blocks/ContentBlock.svelte
@@ -3,7 +3,7 @@
   import Youtube from "svelte-youtube-embed";
 
   import { randomId } from "$lib/utils";
-  import { colourScheme, osStore } from '$lib/store';
+  import { colourScheme, osStore } from "$lib/store";
 
   import Card from "$lib/components/Card.svelte";
   import Tabs from "$lib/components/Tabs.svelte";
@@ -42,7 +42,7 @@
 
   const debounce = (func, wait) => {
     let timeout;
-    return function(...args) {
+    return function (...args) {
       const later = () => {
         clearTimeout(timeout);
         func(...args);
@@ -52,26 +52,35 @@
     };
   };
 
-  $: currentBg = $colourScheme === 'dark' && backgroundDark ? backgroundDark : background;
+  $: currentBg =
+    $colourScheme === "dark" && backgroundDark ? backgroundDark : background;
 
   onMount(() => {
     const handleResize = debounce(() => {
       mobile = window.innerWidth < 768;
-      style = mobile ? "" : currentBg ? `background-image: url(${currentBg});` : "";
+      style = mobile
+        ? ""
+        : currentBg
+          ? `background-image: url(${currentBg});`
+          : "";
     }, 250);
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   });
 
-  $: style = mobile ? "" : currentBg ? `background-image: url(${currentBg});` : "";
+  $: style = mobile
+    ? ""
+    : currentBg
+      ? `background-image: url(${currentBg});`
+      : "";
 
-  osStore.subscribe(data => {
+  osStore.subscribe((data) => {
     if (!data.loading && buttons && imgSrc) {
       buttons = [...data.osButtons];
-      imgSrc = `/assets/media/${data.os}.webp`
+      imgSrc = `/assets/media/${data.os}.webp`;
     }
   });
 </script>
@@ -84,7 +93,7 @@
   {#if title}
     <h1
       class={`text-4xl font-semibold tracking-tight max-w-2xl px-8 mx-auto text-center text-red-berry-900 dark:text-neutral-400
-        ${!boxed ? 'lg:mb-24' : 'lg:mb-8'}`}
+        ${!boxed ? "lg:mb-24" : "lg:mb-8"}`}
     >
       {@html title}
     </h1>
@@ -92,37 +101,42 @@
 
   <div
     class={`mx-auto grid gap-8 px-8 ${classes}
-      ${!boxed ? 'py-8 max-w-screen-lg 2xl:max-w-screen-xl' : 'max-w-screen-md'}
-      ${columns ? 'gap-x-8 lg:gap-x-16 xl:gap-x-32 lg:grid-cols-10' : ''}
-      ${border ? 'border border-mine-shaft-200 dark:border-mine-shaft-800' : ''}`}
+      ${!boxed ? "py-8 max-w-screen-lg 2xl:max-w-screen-xl" : "max-w-screen-md"}
+      ${columns ? "gap-x-8 lg:gap-x-16 xl:gap-x-32 lg:grid-cols-10" : ""}
+      ${border ? "border border-mine-shaft-200 dark:border-mine-shaft-800" : ""}`}
   >
     {#if content || buttons}
-      <div class={columns ? 'col-span-full lg:col-span-4' : 'col-span-full'}>
+      <div class={columns ? "col-span-full lg:col-span-4" : "col-span-full"}>
         {#if content}
           <div
             class={`prose prose-h2:text-lg prose-h1:text-xl prose-headings:font-light prose-headings:tracking-tight
               prose-headings:text-gray-700 prose-headings:dark:text-neutral-300
               prose-p:font-light prose-p:text-base prose-p:text-gray-700 prose-p:dark:text-gray-300
-              ${columns ? 'max-w-full' : 'mt-8 md:mt-24 text-center max-w-2xl mx-auto'}`}
+              ${columns ? "max-w-full" : "mt-8 md:mt-24 text-center max-w-2xl mx-auto"}`}
           >
             <slot />
           </div>
         {/if}
         {#if buttons}
-          <div
-            class={`grid grid-cols-1 md:grid-cols-2 gap-4 items-center mt-8
-              ${!columns ? 'text-center' : ''}`}
-          >
-            {#each buttons as button}
-              <Button {...button} />
-            {/each}
-          </div>
+          {#if buttons.length > 1}
+            <div class={`grid grid-cols-1 gap-4 items-center mt-8 mr-32 ${!columns ? "text-center" : ""}`}>
+              {#each buttons as button}
+                <Button {...button} />
+              {/each}
+            </div>
+          {:else}
+            <div class={`mt-8 mr-32 ${!columns ? "text-center" : ""}`}>
+              {#each buttons as button}
+                <Button {...button} />
+              {/each}
+            </div>
+          {/if}
         {/if}
       </div>
     {/if}
 
     {#if videoId || videoSources || tabs || imgSrc || innerColumns}
-      <div class={columns ? 'col-span-full lg:col-span-6' : 'col-span-full'}>
+      <div class={columns ? "col-span-full lg:col-span-6" : "col-span-full"}>
         {#if videoId}
           <Youtube id={videoId} altThumb={true} --title-font-family="Silka" />
         {:else if videoSources}
@@ -138,7 +152,9 @@
         {:else if tabs}
           <Tabs {tabs} />
         {:else if innerColumns}
-          <div class="max-w-2xl mx-auto flex flex-col gap-8 md:gap-16 mt-2 md:mt-0 md:grid md:grid-cols-2">
+          <div
+            class="max-w-2xl mx-auto flex flex-col gap-8 md:gap-16 mt-2 md:mt-0 md:grid md:grid-cols-2"
+          >
             {#each innerColumns as innerColumn}
               {#if innerColumn.link}
                 <a
@@ -172,7 +188,7 @@
       class={`text-center max-w-2xl mx-auto px-8 mt-8 prose prose-h2:text-xl prose-headings:font-light
               prose-headings:tracking-tight prose-headings:text-gray-700 prose-headings:dark:text-neutral-300
               prose-p:font-light prose-p:text-base prose-p:text-gray-700 prose-p:dark:text-gray-300
-              ${columns ? 'order-first' : ''}`}
+              ${columns ? "order-first" : ""}`}
     >
       {#if $$slots.extraContent}
         <slot name="extraContent" />
@@ -183,7 +199,9 @@
   {/if}
 
   {#if extraImage}
-    <div class={`col-span-10 text-center px-8 mt-8 mb-16 ${!columns ? 'max-w-2xl mx-auto' : 'max-w-full'}`}>
+    <div
+      class={`col-span-10 text-center px-8 mt-8 mb-16 ${!columns ? "max-w-2xl mx-auto" : "max-w-full"}`}
+    >
       {#if extraImageLink}
         <a href={extraImageLink} target="_blank" rel="noopener noreferrer">
           <Image figure={false} imgSrc={extraImage} imgAlt={extraImageAlt} />
