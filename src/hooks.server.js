38812b9c9efa3ca6ea83b107de94a5feb5e40eb5
conradolandia <andresconrado@gmail.com>
2025-02-17 16:21:42 -0500

    Nope, didn't work

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -8,6 +8,21 @@ export async function handle({ event, resolve }) {
   if (lang) {
     locale.set(lang);
   }
+
+  if (
+    event.url.pathname.startsWith("/blog/") &&
+    event.url.pathname.match(
+      /\.(png|jpe?g|gif|svg|webp|webm|mp4|ogv|mp3|ogg)$/i
+    )
+  ) {
+    const imagePath = join(process.cwd(), "src", "routes", event.url.pathname);
+    if (existsSync(imagePath)) {
+      const stream = createReadStream(imagePath);
+      return new Response(stream);
+    }
+  }
+
+  return resolve(event);
 }
 
 /** @type {import('@sveltejs/kit').HandleServerError} */
