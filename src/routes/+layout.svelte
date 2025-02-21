4dcf67f36ed85ebb683b7cc73ae0d4cf2c3b4163
conradolandia <andresconrado@gmail.com>
2025-02-21 17:50:36 -0500

    Add head settings to the page, not the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,12 @@
 <script lang="ts">
   import "../app.css";
 
-  import { waitLocale, _ } from "svelte-i18n";
-
-  import { siteUrl, ogImage } from "$lib/config";
+  import { waitLocale } from "svelte-i18n";
 
   import Header from "$lib/blocks/Header.svelte";
   import Footer from "$lib/blocks/Footer.svelte";
   import Loader from "$lib/components/Loader.svelte";
 </script>
-
-<svelte:head>
-  <title>{$_('config.site.title') || 'Spyder'} | {$_('config.site.subtitle') || 'The Python IDE that scientists and data analysts deserve'}</title>
-  <meta name="description" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
-  <meta name="keywords" content={'Python, IDE, Scientific Computing, Data Analysis, Spyder, Jupyter, PyCharm, VSCode'} />
-  <meta name="author" content={$_('config.site.author') || 'Spyder Website Contributors'} />
-  <link rel="canonical" href={siteUrl} />
-  <meta property="og:title" content={$_('config.site.title') || 'Spyder'} />
-  <meta property="og:description" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
-  <meta property="og:url" content={siteUrl} />
-  <meta property="og:site_name" content={$_('config.site.title') || 'Spyder'} />
-  <meta property="og:type" content="website" />
-  <meta property="og:image" content={ogImage} />
-  {#if ogImage.startsWith('https')}
-    <meta property="og:image:secure_url" content={ogImage} />
-  {/if}
-  <meta property="og:image:alt" content={$_('config.site.title') || 'Spyder'} />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="630" />
-  <meta property="og:locale" content="en_US" />
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="@spyder_ide" />
-  <meta name="twitter:creator" content="@spyder_ide" />
-  <meta name="twitter:title" content={$_('config.site.title') || 'Spyder'} />
-  <meta name="twitter:description" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
-  <meta name="twitter:image" content={ogImage} />
-  <meta name="twitter:image:alt" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
-</svelte:head>
 
 {#await waitLocale()}
   <div class="layout grid h-full">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { _, json } from "svelte-i18n";
-  import { config, frontpage } from "$lib/config";
+  import { config, frontpage, siteUrl, ogImage } from "$lib/config";
   import { mergeContentBlocks } from "$lib/utils/content";
 
   import Hero from "$lib/blocks/Hero.svelte";
@@ -30,6 +30,34 @@
     }
   }
 </script>
+
+<svelte:head>
+  <title>{$_('config.site.title') || 'Spyder'} | {$_('config.site.subtitle') || 'The Python IDE that scientists and data analysts deserve'}</title>
+  <meta name="description" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
+  <meta name="keywords" content={'Python, IDE, Scientific Computing, Data Analysis, Spyder, Jupyter, PyCharm, VSCode'} />
+  <meta name="author" content={$_('config.site.author') || 'Spyder Website Contributors'} />
+  <link rel="canonical" href={siteUrl} />
+  <meta property="og:title" content={$_('config.site.title') || 'Spyder'} />
+  <meta property="og:description" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
+  <meta property="og:url" content={siteUrl} />
+  <meta property="og:site_name" content={$_('config.site.title') || 'Spyder'} />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content={ogImage} />
+  {#if ogImage.startsWith('https')}
+    <meta property="og:image:secure_url" content={ogImage} />
+  {/if}
+  <meta property="og:image:alt" content={$_('config.site.title') || 'Spyder'} />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+  <meta property="og:locale" content="en_US" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@spyder_ide" />
+  <meta name="twitter:creator" content="@spyder_ide" />
+  <meta name="twitter:title" content={$_('config.site.title') || 'Spyder'} />
+  <meta name="twitter:description" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
+  <meta name="twitter:image" content={ogImage} />
+  <meta name="twitter:image:alt" content={$_('config.site.description') || 'Get the ease of use of Jupyter along with many advanced features found in PyCharm and VSCode in a single programming environment'} />
+</svelte:head>
 
 <Hero id="hero-section" divider={true} />
 
