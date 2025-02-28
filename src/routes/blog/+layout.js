a7909f24f74a3d9a250bb4315c6c82d9e977e475
conradolandia <andresconrado@gmail.com>
2025-02-28 13:55:39 -0500

    Try again converting the site into an SSG/SPA

--- src/routes/blog/+layout.js
+++ src/routes/blog/+layout.js
@@ -2,7 +2,7 @@ export const load = async ({ url, route }) => {
     const path = url.pathname;
     const slug = path.split('/').filter(Boolean).pop();
 
-    console.log("From layout:", `slug: ${slug}, url: ${url.href}`);
+    //console.log("From layout:", `slug: ${slug}, url: ${url.href}`);
 
     return {
         url: url.href,
