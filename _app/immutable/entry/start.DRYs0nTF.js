74396fd4189a67587c20d9a6bc52a57a3c3f6866
ccordoba12 <ccordoba12@gmail.com>
2026-02-27 21:01:18 +0000

    Deploying to gh-pages from @ spyder-ide/spyder-website@e0e0f9104e79d4c9619b0f1a02971978e7d283bf 🚀

---
+++ _app/immutable/entry/start.DRYs0nTF.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/B4nXRfnl.js";import{A as t}from"../chunks/CN66MQKc.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.QKx8dQLo.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DvcRm01G.js";import{A as t}from"../chunks/BRomHLPs.js";export{t as load_css,r as start};
